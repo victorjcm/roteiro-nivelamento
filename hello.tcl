7b272d9bd9bbb8c852c39dcd16a7900e57bbf29d
Victor Jose <victorjcmedeiros@gmail.com>
2026-03-20 20:00:14 -0300

    criando arquivo na branch TCL_hello

---
+++ hello.tcl
@@ -0,0 +1,8 @@
+puts "Olá, Mundo - Entre aspas" ; #Este é um comentário após o comando.
+# Este é um comentário no início de uma linha
+puts {olá, Mundo - Entre chaves}
+puts {Exemplo de sintaxe de comentário inválida} #*Erro* - não há ponto e virgula!
+puts "Esta é a linha 1"; puts "esta é a linha 2"
+puts "Olá, Mundo; - Com um ponto e vírgula dentro das aspas"
+# Palavras não precisam ser colocadas entre aspas, a menos que contenham e....
+puts OláMundo
