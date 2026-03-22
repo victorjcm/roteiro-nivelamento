39cba48137c4af726a4a0398b2862d73ae383d46
Emilieny Silva <emilieny48@gamil.com>
2026-03-22 17:28:21 -0300

    lendo arquivo contador_netlist.tcl

--- relatorio.tcl
+++ relatorio.tcl
@@ -1,32 +1,8 @@
-puts " === HIERARQUIA DO DESIGN ==="
+set contador [open "contador_netlist.tcl" r]
 
-package require Tk
+while {[gets $contador linha] != -1} {
+	puts $linha
+	
+	}
 
-# 1. Criar o widget
-ttk::treeview .tree -show tree
-pack .tree -fill both -expand true
-
-# 2. Criar o nó PAI (o primeiro argumento {} significa que ele não tem pai, ou seja, é raiz)
-# Guardamos o ID do pai na variável 'idPai'
-set idSomador [.tree insert {} end -text "somador_4bits"]
-
-# 3. Criar os FILHOS dentro do PAI (passamos '$idPai' como o primeiro argumento)
-.tree insert $idSomador end -text "(apenas células primitivas"
-
-
-set idContador [.tree insert {} end -text "contador_4bits"]
-
-.tree insert $idContador end -text "flipflop_D (4 instancias)"
-.tree insert $idContador end -text "(celulas primitivas)"
-
-set idFlipflop_D [.tree insert {} end -text "flipflop_D"]
-.tree insert $idFlipflop_D end -text "(celulas primitivas)"
-
-
-
-
-# Dica: Abrir a árvore automaticamente para mostrar os filhos
-.tree item $idSomador -open true
-.tree item $idContador -open true
-.tree item $idFlipflop_D -open true
-
+close $contador
